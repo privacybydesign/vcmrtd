b8f8e977021b475728b47fe51d927c8e0dcbb9bd
Dibran Mulder <dibran.mulder@outlook.com>
2025-07-25 10:50:06 +0200

    Moved theming from lib to example app

---
+++ example/lib/theme/theme.dart
@@ -0,0 +1,20 @@
+// Core theme components
+export 'app_colors.dart';
+export 'app_text_styles.dart';
+export 'app_spacing.dart';
+export 'app_theme.dart';
+
+// Re-export commonly used Flutter classes for convenience
+export 'package:flutter/material.dart' show 
+  ThemeData,
+  ColorScheme,
+  TextTheme,
+  TextStyle,
+  Color,
+  Colors,
+  EdgeInsets,
+  BorderRadius,
+  BoxDecoration,
+  Container,
+  SizedBox,
+  Padding;
