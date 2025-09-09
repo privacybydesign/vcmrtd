1c596f37bc86f7f0a5e37e7d4ab9b33b9e0d04cd
Dibran Mulder <d.mulder@caesar.nl>
2025-09-09 15:23:35 +0200

    Bumped archive dep from 3.x to 4.x

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vcmrtd/main.dart';
+import 'package:vcmrtdapp/main.dart';
 
 void main() {
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
