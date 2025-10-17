e41ab9379b20d59136dc0a3a935ac9a7154d0a16
saravahdatipour <saravahdatipour@gmail.com>
2025-10-17 15:50:43 +0200

    Add both AIDs

--- lib/src/lds/df1/df1.dart
+++ lib/src/lds/df1/df1.dart
@@ -5,6 +5,7 @@ import 'package:vcmrtd/extensions.dart';
 class DF1 {
   // See: Section 3.1 https://www.icao.int/publications/Documents/9303_p10_cons_en.pdf
   // ignore: non_constant_identifier_names
-  static final AID  = "A0000002471001".parseHex();
+  static final PassportAID  = "A0000002471001".parseHex();
+  static final DriverAID = "A00000045645444C2D3031".parseHex();
   static const name = "eMRTD Application";
 }
