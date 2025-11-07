481e552bb68438f9cb513bab1044c914e85303ef
saravahdatipour <saravahdatipour@gmail.com>
2025-11-07 15:14:08 +0100

    Format

--- example/lib/providers/reader_providers.dart
+++ example/lib/providers/reader_providers.dart
@@ -5,47 +5,40 @@ import 'package:vcmrtdapp/widgets/common/scanned_mrz.dart';
 
 final passportReaderProvider = StateNotifierProvider.autoDispose
     .family<DocumentReader<PassportData>, DocumentReaderState, ScannedPassportMRZ>((ref, scannedPassportMRZ) {
-  final nfc = NfcProvider();
-  final accessKey = DBAKey(
-    scannedPassportMRZ.documentNumber,
-    scannedPassportMRZ.dateOfBirth,
-    scannedPassportMRZ.dateOfExpiry,
-  );
-  final dgReader = DataGroupReader(nfc, DF1.PassportAID, accessKey);
-  final parser = PassportParser();
-  final docReader = DocumentReader(
-    parser,
-    dgReader,
-    nfc,
-    DocumentType.passport,
-    settings: DocumentReaderSettings(
-      skipDataGroups: {'DG11'},
-    ),
-  );
+      final nfc = NfcProvider();
+      final accessKey = DBAKey(
+        scannedPassportMRZ.documentNumber,
+        scannedPassportMRZ.dateOfBirth,
+        scannedPassportMRZ.dateOfExpiry,
+      );
+      final dgReader = DataGroupReader(nfc, DF1.PassportAID, accessKey);
+      final parser = PassportParser();
+      final docReader = DocumentReader(
+        parser,
+        dgReader,
+        nfc,
+        DocumentType.passport,
+        settings: DocumentReaderSettings(skipDataGroups: {'DG11'}),
+      );
 
-  ref.onDispose(docReader.cancel);
-  return docReader;
-});
+      ref.onDispose(docReader.cancel);
+      return docReader;
+    });
 
 final passportUrlProvider = StateProvider((ref) => '');
 
 final drivingLicenceReaderProvider = StateNotifierProvider.autoDispose
     .family<DocumentReader<DrivingLicenceData>, DocumentReaderState, ScannedDriverLicenseMRZ>((
-    ref,
-    scannedDriverLicenceMRZ,
+      ref,
+      scannedDriverLicenceMRZ,
     ) {
-  final nfc = NfcProvider();
-  final accessKey = CanKey(scannedDriverLicenceMRZ.documentNumber, DocumentType.driverLicense);
-  final dgReader = DataGroupReader(nfc, DF1.DriverAID, accessKey);
-  final parser = DrivingLicenceParser();
-  final docReader = DocumentReader(
-    parser,
-    dgReader,
-    nfc,
-    DocumentType.driverLicense,
-  );
+      final nfc = NfcProvider();
+      final accessKey = CanKey(scannedDriverLicenceMRZ.documentNumber, DocumentType.driverLicense);
+      final dgReader = DataGroupReader(nfc, DF1.DriverAID, accessKey);
+      final parser = DrivingLicenceParser();
+      final docReader = DocumentReader(parser, dgReader, nfc, DocumentType.driverLicense);
 
-  ref.onDispose(docReader.cancel);
+      ref.onDispose(docReader.cancel);
 
-  return docReader;
-});
+      return docReader;
+    });
