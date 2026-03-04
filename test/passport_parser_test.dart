a09913aaa351119dcc06c923cb805d6fc67eb732
Dibran Mulder <d.mulder@caesar.nl>
2026-03-04 11:29:52 +0100

    Fix formatting

--- test/passport_parser_test.dart
+++ test/passport_parser_test.dart
@@ -13,36 +13,24 @@ void main() {
 
     test('invalid DG11 tag throws EfParseError', () {
       final parser = PassportParser();
-      expect(
-        () => parser.parseDG11(parseTestCase(invalidDg11Tag)),
-        throwsA(isA<EfParseError>()),
-      );
+      expect(() => parser.parseDG11(parseTestCase(invalidDg11Tag)), throwsA(isA<EfParseError>()));
     });
 
     test('invalid DG11 missing tag list throws EfParseError', () {
       final parser = PassportParser();
-      expect(
-        () => parser.parseDG11(parseTestCase(invalidDg11MissingTagList)),
-        throwsA(isA<EfParseError>()),
-      );
+      expect(() => parser.parseDG11(parseTestCase(invalidDg11MissingTagList)), throwsA(isA<EfParseError>()));
     });
 
     test('empty bytes throws error', () {
       final parser = PassportParser();
-      expect(
-        () => parser.parseDG11(Uint8List(0)),
-        throwsA(anything),
-      );
+      expect(() => parser.parseDG11(Uint8List(0)), throwsA(anything));
     });
   });
 
   group('DG12 parsing', () {
     test('invalid DG12 tag throws EfParseError', () {
       final parser = PassportParser();
-      expect(
-        () => parser.parseDG12(parseTestCase(invalidDg12Tag)),
-        throwsA(isA<EfParseError>()),
-      );
+      expect(() => parser.parseDG12(parseTestCase(invalidDg12Tag)), throwsA(isA<EfParseError>()));
     });
   });
 }
